1eaa1aaab27ae4ddf1aa575cbacd1dee9b562c68
jaeyoung <kjy0302014@gmail.com>
2023-09-13 20:37:13 +0900

    initial backend commit

---
+++ backend/src/test/java/com/develog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
