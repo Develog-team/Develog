4c9c8d69c4752a65a82d9f419c08488a5588e08d
sujin <yhs04378@gmail.com>
2023-09-19 15:43:30 +0900

    feat: 레이아웃구성 및 기본 nav 추가

--- fontend/src/layouts/MainLayout.tsx
+++ fontend/src/layouts/MainLayout.tsx
@@ -1,11 +1,18 @@
 import { Layout } from "antd"
+import { Content } from "antd/es/layout/layout"
+import { MainFooter, MainHeader } from "components"
 import { Outlet } from "react-router-dom"
 
 export const MainLayout = () => {
 
     return (
-        <Layout>
-            <Outlet />
+        <Layout
+            style={{ width: '100vw', height: '100vh' }}>
+            <MainHeader />
+            <Content>
+                <Outlet />
+            </Content>
+            <MainFooter />
         </Layout>
     )
 }
