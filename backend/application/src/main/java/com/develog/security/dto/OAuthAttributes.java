2fd522f7ed0c4a3b1e45a8b1da9d99966bdcb6f4
hyungjin <hyungzin0309@naver.com>
2023-09-26 12:39:34 +0900

    [Feature] OAuth 구현

--- backend/application/src/main/java/com/develog/security/dto/OAuthAttributes.java
+++ backend/application/src/main/java/com/develog/security/dto/OAuthAttributes.java
@@ -1,30 +1,30 @@
 package com.develog.security.dto;
 
-import com.develog.member.Member;
-import com.develog.member.Role;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
 @Getter
+@Builder
 public class OAuthAttributes {
     private Map<String, Object> attributes;
     private String nameAttributeKey;
     private String name;
+    private String nickname;
     private String email;
     private String picture;
 
-    @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
-        this.attributes = attributes;
-        this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
-        this.email = email;
-        this.picture = picture;
-    }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        switch (registrationId) {
+            case "google":
+                return ofGoogle(userNameAttributeName, attributes);
+            case "kakao":
+                return ofKakao(userNameAttributeName, attributes);
+            case "naver":
+                return ofNaver(userNameAttributeName, attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -38,12 +38,26 @@ public class OAuthAttributes {
                 .build();
     }
 
-    public Member toEntity(){
-        return Member.builder()
-                .name(name)
-                .email(email)
-                .picture(picture)
-                .role(Role.GUEST)
+    public static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        return OAuthAttributes.builder()
+                .name((String) properties.get("nickname"))
+                .picture((String) properties.get("profile_image"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) properties.get("name"))
+                .nickname((String) properties.get("nickname"))
+                .picture((String) properties.get("profile_image"))
+                .email((String) properties.get("email"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
 }
