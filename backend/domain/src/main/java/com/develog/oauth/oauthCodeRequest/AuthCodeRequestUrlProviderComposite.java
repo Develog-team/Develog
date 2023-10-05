07397828118c4729dd3200634f0d9887f327207a
hyungjin <hyungzin0309@naver.com>
2023-10-05 20:34:53 +0900

    [feature] Auth token request url provider, login interface 구현

---
+++ backend/domain/src/main/java/com/develog/oauth/oauthCodeRequest/AuthCodeRequestUrlProviderComposite.java
@@ -0,0 +1,25 @@
+package com.develog.oauth.oauthCodeRequest;
+
+import com.develog.oauth.OauthType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toMap;
+
+
+@Component
+public class AuthCodeRequestUrlProviderComposite {
+
+    private final Map<OauthType, AuthCodeRequestUrlProvider> providersByType;
+
+    public AuthCodeRequestUrlProviderComposite(List<AuthCodeRequestUrlProvider> providers){
+        providersByType = providers.stream().collect(toMap(AuthCodeRequestUrlProvider::support, Function.identity()));
+    }
+
+    public String getOauthCodeRequestUrl(OauthType type){
+        return providersByType.get(type).getUrl();
+    }
+}
