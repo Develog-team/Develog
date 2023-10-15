fde15a47ec1e4bd14c1d0a87aefffa77413c7b81
rudgus22 <80056046+rudgusee@users.noreply.github.com>
2023-10-15 21:16:25 +0900

    [fix]: profile 로직 제거

--- backend/domain/src/main/java/com/develog/profile/repository/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.develog.profile.repository;
-
-import com.develog.profile.entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
