09a00b9ccabfe3c225f32a232eaee998a7d186a2
hyungjin <hyungzin0309@naver.com>
2023-10-22 21:21:54 +0900

    [feat] record 엔티티 생성

--- backend/domain/src/main/java/com/develog/domain/goal/Goal.java
+++ backend/domain/src/main/java/com/develog/domain/goal/Goal.java
@@ -1,11 +1,15 @@
 package com.develog.domain.goal;
 
 import com.develog.domain.base.UserBaseEntity;
+import com.develog.domain.goal.record.Record;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,4 +26,13 @@ public class Goal extends UserBaseEntity {
 
     @Enumerated(value = EnumType.STRING)
     private GoalStatus status;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "goal_id")
+    private List<Record> records = new ArrayList<>();
+
+    public void addRecord(Record record){
+        records.add(record);
+    }
+
 }
--- backend/domain/src/main/java/com/develog/domain/goal/record/RecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.develog.domain.goal.record;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
